07f51864582a2d7b95353841c1c7b8a833b8ed1e
vctrhugoop <vhdestroy@hotmail.com>
2025-03-03 15:36:41 -0300

    Reapply "🎉 init: start project config"
    This reverts commit d7459532055be464ea3fd2e60da6db6dbd5787cb.

---
+++ src/server.ts
@@ -0,0 +1,34 @@
+import { fastify } from 'fastify';
+import { fastifyCors } from '@fastify/cors';
+import { validatorCompiler, serializerCompiler, ZodTypeProvider, jsonSchemaTransform } from 'fastify-type-provider-zod';
+import { fastifySwagger } from '@fastify/swagger';
+import { fastifySwaggerUi } from '@fastify/swagger-ui';
+import { subscribeToEventRoute } from './routes/subscribe-to-event-route';
+
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors)
+
+app.register(fastifySwagger, {
+  openapi:{
+    info: {
+      title: 'NLW Connect',
+      version: '1.0.0'
+    }
+  },
+  transform: jsonSchemaTransform
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(subscribeToEventRoute)
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('Server is running on port 3333')
+})
