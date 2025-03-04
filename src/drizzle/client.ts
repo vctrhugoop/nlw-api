d4a836ee6c44702dfdabb5415f031d12d0f65285
vctrhugoop <vhdestroy@hotmail.com>
2025-03-03 21:13:21 -0300

    ✨ feat: created table in db

--- src/drizzle/client.ts
+++ src/drizzle/client.ts
@@ -1,12 +1,11 @@
 import postgres from 'postgres';
-<<<<<<< HEAD
 import { drizzle } from 'drizzle-orm/postgres-js'
 import { env } from '../env';
+import { subscriptions } from './schema/subscriptions';
 
 export const pg = postgres(env.POSTGRES_URL)
-export const db = drizzle(pg)
-=======
-import { env } from '../env';
-
-export const pg = postgres(env.POSTGRES_URL)
->>>>>>> 5ad998e71d85e64292f7264ef7a8e4adffe6d9e0
+export const db = drizzle(pg, {
+ schema: {
+  subscriptions
+ }
+})
